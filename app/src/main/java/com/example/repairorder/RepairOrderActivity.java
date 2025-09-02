d98a48a164cb26ee7877f9009bd081e8845f366f
Daron <daronbaltazar@gmail.com>
2025-09-02 12:17:34 -0500

    Example Java addition
    Example Java addition

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -1,6 +1,8 @@
 package com.example.repairorder;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.*;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class RepairOrderActivity extends AppCompatActivity {
+
+    double numbers = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_repair_order);
@@ -22,7 +30,18 @@ public class RepairOrderActivity extends AppCompatActivity {
         });
 
 
+        TextView totalTV = findViewById(R.id.TotalOutput); // How we find the reference to a widget within a ui
 
+        Random gen = new Random();
+
+        double number = gen.nextDouble();
+        String v1 = "$ " + number;
+
+        totalTV.setText(v1);
+
+        String value = totalTV.getText().toString();
+
+        Log.i("TEST", value);
 
 
 
