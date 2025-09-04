376aeac84a65ffc847b9391ff069023962faac5b
Daron <daronbaltazar@gmail.com>
2025-09-04 12:15:42 -0500

    Tried to do math - saved info from edittext widgets.

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -18,6 +18,14 @@ public class RepairOrderActivity extends AppCompatActivity {
     double numbers = 0.0;
     Button submitB; //1. Create Button
     TextView subtotalTV;
+    TextView taxTV;
+    TextView totalTV;
+
+    EditText orderET;
+    EditText techET;
+    EditText paintET;
+    EditText partsET;
+    EditText laborET;
 
     View.OnClickListener buttonListener = new View.OnClickListener() { //2. Create Anonymous Listener
         @Override
@@ -32,8 +40,32 @@ public class RepairOrderActivity extends AppCompatActivity {
             //String value = subtotalTV.getText().toString();
 
             //Log.i("TEST", value);
+
+            String orderTypeValue = orderET.getText().toString();
+            String techTypeValue = techET.getText().toString();
+            //Integer it = Integer.getInteger(orderTypeValue);
+            //Double et = Double.parseDouble(OrderTypeValue);
+            Double partsTypeValue = Double.parseDouble(orderTypeValue);
+            Double paintTypeValue = Double.parseDouble(orderTypeValue);
+            Double laborTypeValue = Double.parseDouble(orderTypeValue);
+
+            double sum = 0;
+
+            sum = partsTypeValue + paintTypeValue + laborTypeValue;
+
+            String sums = "$ " +sum;
+
+
+            totalTV.setText(sums);
+
+
+
+
         }
     };
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +81,18 @@ public class RepairOrderActivity extends AppCompatActivity {
 
 
          subtotalTV = findViewById(R.id.SubTotalOutput); // How we find the reference to a widget within a ui
+        taxTV = findViewById(R.id.TaxOutput);
+        totalTV = findViewById(R.id.TotalOutput);
 
 
         submitB = findViewById(R.id.submitButton);
         submitB.setOnClickListener(buttonListener); //3. Register listener to the button
+
+        orderET = findViewById(R.id.OrderInput);
+        techET = findViewById(R.id.TechnicianInput);
+        paintET = findViewById(R.id.PaintInput);
+        partsET = findViewById(R.id.PartsInput);
+        laborET = findViewById(R.id.LaborInput);
 
 
 
