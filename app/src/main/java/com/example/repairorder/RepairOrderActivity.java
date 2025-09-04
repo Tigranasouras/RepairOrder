22c8edb58a77d9142b62c9e75f748bb446751a4c
Daron <daronbaltazar@gmail.com>
2025-09-04 11:47:03 -0500

    Button Listener
    Button Listener - changes subtotal with random number

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -2,6 +2,7 @@ package com.example.repairorder;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.*;
 
 import androidx.activity.EdgeToEdge;
@@ -15,11 +16,27 @@ import java.util.Random;
 public class RepairOrderActivity extends AppCompatActivity {
 
     double numbers = 0.0;
+    Button submitB; //1. Create Button
+    TextView subtotalTV;
+
+    View.OnClickListener buttonListener = new View.OnClickListener() { //2. Create Anonymous Listener
+        @Override
+        public void onClick(View v) {
+            Random gen = new Random();
+
+            double number = gen.nextDouble();
+            String v1 = "$ " + number;
+
+            subtotalTV.setText(v1);
+
+            //String value = subtotalTV.getText().toString();
+
+            //Log.i("TEST", value);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_repair_order);
@@ -30,18 +47,16 @@ public class RepairOrderActivity extends AppCompatActivity {
         });
 
 
-        TextView totalTV = findViewById(R.id.TotalOutput); // How we find the reference to a widget within a ui
 
-        Random gen = new Random();
+         subtotalTV = findViewById(R.id.SubTotalOutput); // How we find the reference to a widget within a ui
 
-        double number = gen.nextDouble();
-        String v1 = "$ " + number;
 
-        totalTV.setText(v1);
+        submitB = findViewById(R.id.submitButton);
+        submitB.setOnClickListener(buttonListener); //3. Register listener to the button
 
-        String value = totalTV.getText().toString();
 
-        Log.i("TEST", value);
+
+
 
 
 
